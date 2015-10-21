be00d344af656947f0d7e1f3bb3937cfd4ffcdb5
xiangxiyun <heidi.xxy@hotmail.com>
2015-10-21 17:54:31 +0800

    update the submenu

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
-	$(".MenuElement").click(function(){	
-			if ($(this).children(".SubMenu").css("display") == "none") {
-				$(this).children("a").css("background-color", "#4682B4");
-				$(this).children("a").css("color", "#FFF");
-				$(this).children(".SubMenu").slideDown("fast");
-				$(this).children(".SubMenu").css("display") = "inline";
+	$(".reference").click(function(){	
+			if ($(this).parent().children(".SubMenu").css("display") == "none") {
+				//$(this).children("a").css("background-color", "#4682B4");
+				//$(this).children("a").css("color", "#FFF");
+				$(this).parent().children("a").addClass("substainColor");
+				$(this).parent().children(".SubMenu").slideDown("fast");
+				$(this).parent().children(".SubMenu").css("display") = "inline";
 			}
 			else {
-				$(this).children(".SubMenu").slideUp("fast");
-				$(this).children("a").css("background-color", "#cccccc");
-				$(this).children("a").css("color", "#333333");	
-				$(this).children(".SubMenu").css("display") = "none";
+				$(this).parent().children(".SubMenu").slideUp("fast");
+				//$(this).children("a").css("background-color", "#cccccc");
+				//$(this).children("a").css("color", "#333333");	
+				if ($(this).parent().children("a").hasClass("substainColor")) {
+					$(this).parent().children("a").removeClass("substainColor");
+				};
+				$(this).parent().children(".SubMenu").css("display") = "none";
 			}
 		
   });
