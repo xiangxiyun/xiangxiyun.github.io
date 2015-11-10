fdc60d796993416f5c2434415c45d7425e5643b5
xiangxiyun <heidi.xxy@hotmail.com>
2015-11-10 17:14:47 +0800

    change most of the pages

--- javascripts/sidebar.js
+++ javascripts/sidebar.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 	$(".MenuElement").click(function(){
 		if ($(this).hasClass("substainColor")) {
+
 			$(this).children('.SubMenu').slideUp();
+			$($(this).children('.reference')).removeClass("substainColor");
 			$(this).removeClass("substainColor");
 		}
 		else {
 			$(this).children('.SubMenu').slideDown();
+			$($(this).children('.reference')).addClass("substainColor");
 			$(this).addClass("substainColor");
 		};
 		
